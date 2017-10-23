964c566f824260e100448824dc4c9e19fd2e84c1
Evangeline Ireland <eireland@concord.org>
2017-10-23 11:51:00 -0700

    Removed redundant wait function

--- learn_component_objects/portal_main_object.rb
+++ learn_component_objects/portal_main_object.rb
@@ -1,8 +1,8 @@
-require '../base_object'
-require './login_modal_object'
-require './portal_landing_object'
-require './learn_header_object'
-require './home_page_object'
+require '~/development/learn_smoke_test/base_object'
+require './learn_component_objects/login_modal_object'
+require './learn_component_objects/portal_landing_object'
+require './learn_component_objects/learn_header_object'
+require './learn_component_objects/home_page_object'
 
 class PortalMainObject < BaseObject
   include PortalLandingObject
@@ -13,13 +13,6 @@ class PortalMainObject < BaseObject
 
   def initialize()
     puts "Initializing"
-  end
-
-  private
-
-  def wait_for(seconds=25)
-    puts "Waiting"
-    Selenium::WebDriver::Wait.new(:timeout => seconds).until { yield }
   end
 
 end
